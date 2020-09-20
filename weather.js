4cf78706bd6236c39ad990a80a0e16b4ea85ef8f
haeunsong <jamy0225@naver.com>
2020-09-20 22:49:32 +0900

    날씨 API 가져와서 적용시키기!!

--- weather.js
+++ weather.js
@@ -1,8 +1,25 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "bfde9481fa4b074aa945931f60dd8eb8";
 const COORDS = "coords";
 
+function getWeather(lat,lng){
+    // 서버로부터 데이터가 들어올 때까지 기다리기
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){
+        return response.json(); // 일단 정보를 가져오고
+    })
+    .then(function(json){ // 끌어다쓴다.
+        const temperature = json.main.temp; // console로 해서 데이터확인하면 main의 temp부분의 온도가 있다.
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
 }
+// 좌표가 없을 때만 실행됨.
 function handleGeoSuccess(position){
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -11,6 +28,7 @@ function handleGeoSuccess(position){
         longitude   // longitude:longitude 와 같은뜻(양쪽이 같을 떄는 하나만 써도 OK)
     };
     saveCoords(coordsObj)
+    getWeather(latitude,longitude);
 }
 function handleGeoError(){
     console.log("Cant access geo location")
@@ -19,11 +37,12 @@ function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSuccess,handleGeoError);
 }
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords===null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords===null){
         askForCoords();
     }else{
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
     }
 }
 function init(){
